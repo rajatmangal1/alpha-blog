9dd5194ea83033cfdf67469d8b150636e3ad25c5
Nipun Jindal <njindal@sfu.ca>
2018-02-13 23:29:28 +0000

    Added the articles listing and links among the pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,11 +3,16 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params());
     
-    if (@article.save)
+    if (@article.save)    # If validations are successfull
       flash[:notice] = "Article was successfully created"
       redirect_to article_path(@article) ## @artcile is passed in because article_path (show fn) needs the id (can see in rake routes)
     else
@@ -15,9 +20,26 @@ class ArticlesController < ApplicationController
     end
   end
   
+  def update
+    @article = Article.find(params[:id])
+    if (@article.update(article_params()))
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+  
+  def index
+    @article = Article.all
+    
+  end
+  
   def show
     @article = Article.find(params[:id])
   end
+  
+  
   
   
   
