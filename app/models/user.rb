89e24c0848c3b1bb0ecf160ff7a9f59acd747c1c
Nipun Jindal <njindal@sfu.ca>
2018-02-14 03:30:08 +0000

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
-  
+  validates :username, presence: true, 
+            length: {minimum: 3, maximum: 25}, 
+            uniqueness: {case_sensitive: false}
+  VALID_REGEX_EXPRESSION = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, 
+            uniqueness: {case_sensitive: false},
+            length: {maximum: 105},
+            format: {with: VALID_EMAIL_REGEX}
 end
